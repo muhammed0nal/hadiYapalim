6ed89c2ef751fe3e779a59eb9b55d120058e5484
muhammed0nal <onalmuhammed11@gmail.com>
2025-02-03 23:02:57 +0300

    İlk yükleme

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { DrawerNavigator } from "./Navigations/DrawerNavigator/DrawerNavigator";
 
-// Contextss
+// Contexts
 import { AuthProvider, useAuth } from "./contexts/AuthContext";
 
 // Screens
